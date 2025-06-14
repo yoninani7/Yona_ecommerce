222f526cba38035a25eda4b8442738a43db9a16d
yoninani7 <110281935+yoninani7@users.noreply.github.com>
2025-06-14 09:09:22 +0300

    Update main.dart

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -11,10 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 255, 5, 5),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Stadium'),
     );
   }
 }
