ca0fc450c43124be7c2d168f07461f4e3e039a95
yoninani7 <yonsbreak7@gmail.com>
2025-06-20 13:54:22 +0300

    Update main.dart

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[const Text('Hello World!')],
+          children: <Widget>[const Text('no stadiums found  :(  ')],
         ),
       ),
     );
